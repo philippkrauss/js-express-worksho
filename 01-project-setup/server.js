81b5293afcb890080d619e802aea5cef4fcbec31
Philipp Krauss <philipp.krauss@codecentric.de>
2022-06-09 07:56:47 +0200

    Update simple example with possibility to use random port

--- 01-project-setup/server.js
+++ 01-project-setup/server.js
@@ -12,6 +12,6 @@ if (process.argv[2]) {
   port = parseInt(process.argv[2])
 }
 // set port
-app.listen(port, () => {
-  console.log(`Node app is running on port ${port}`)
+const server = app.listen( port,() => {
+  console.log(`Node app is running on port ${server.address().port}`)
 })
