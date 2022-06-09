a6ef5f7276ab6e94b5c1a4c5e88ee3860eff6652
Philipp Krauss <philipp.krauss@codecentric.de>
2022-06-09 10:07:16 +0200

    Make port configurable via environment variable

--- 01-project-setup/server.js
+++ 01-project-setup/server.js
@@ -11,6 +11,9 @@ let port = 3000
 if (process.argv[2]) {
   port = parseInt(process.argv[2])
 }
+if (process.env['PORT']) {
+  port = parseInt(process.env['PORT'])
+}
 // set port
 const server = app.listen( port,() => {
   console.log(`Node app is running on port ${server.address().port}`)
